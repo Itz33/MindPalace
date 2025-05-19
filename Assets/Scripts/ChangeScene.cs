8188a5cf3219d72506b9767ed86fcb643a27f175
Alejandro Jose Garcia Alfaro <alejandrojosegarcia75@gmail.com>
2025-05-19 03:09:22 -0600

    Kitchen loads only when player  has newspaper

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 using static SceneEnumAssigner;
 
@@ -9,11 +10,48 @@ public class ChangeScene : MonoBehaviour
     [SerializeField] private Vector3 startPosition;
     [SerializeField] playerPosition newplayerPosition;
     [SerializeField] targetScene sceneToChange;
+    
+    [Header("Newspaper Check")]
+    [SerializeField] private bool requiresNewspaper = false;
+    [SerializeField] private float messageDisplayTime = 2f;
+    [SerializeField] private GameObject messageUI; // Optional: Assign a UI panel in Inspector
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        newplayerPosition.position = startPosition;
-        SceneManager.LoadScene((int)sceneToChange);
+        if (collision.CompareTag("Player"))
+        {
+            if (requiresNewspaper && !NewspaperItem.NewspaperPickedUp)
+            {
+                ShowMessage("You need to read the newspaper first!");
+                return;
+            }
+            
+            newplayerPosition.position = startPosition;
+            SceneManager.LoadScene((int)sceneToChange);
+        }
     }
 
+    private void ShowMessage(string message)
+    {
+        Debug.Log(message);
+        
+        // If you have UI for messages
+        if (messageUI != null)
+        {
+            if (messageUI.TryGetComponent<TextMeshProUGUI>(out var textComponent))
+            {
+                textComponent.text = message;
+            }
+            messageUI.SetActive(true);
+            Invoke("HideMessage", messageDisplayTime);
+        }
+    }
+
+    private void HideMessage()
+    {
+        if (messageUI != null)
+        {
+            messageUI.SetActive(false);
+        }
+    }
 }
--- Assets/Scripts/NewspaperItem.cs
+++ Assets/Scripts/NewspaperItem.cs
@@ -4,14 +4,23 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 public class NewspaperItem : MonoBehaviour
 {
    public float interactionDistance = 2f;
    private GameObject player;
    private bool isNear = false;
-   
+    
    public TMP_Text newspaperText;
    public Image newspaperImage;
+    
+   // Static property to track pickup status
+   public static bool NewspaperPickedUp { get; private set; } = false;
 
    void Start()
    {
@@ -23,16 +32,23 @@ public class NewspaperItem : MonoBehaviour
    void Update()
    {
       if (player == null) return;
-         //float check the distance with the player
-         float distance = Vector3.Distance(transform.position, player.transform.position);
-         isNear = distance <= interactionDistance;
+        
+      float distance = Vector3.Distance(transform.position, player.transform.position);
+      isNear = distance <= interactionDistance;
 
-         if (isNear && Input.GetKeyDown(KeyCode.E))
-         {
-            if (newspaperText != null) newspaperText.gameObject.SetActive(true);
-            if (newspaperImage != null) newspaperImage.gameObject.SetActive(true);
-            Destroy(gameObject);
-         }
+      if (isNear && Input.GetKeyDown(KeyCode.E))
+      {
+         PickUpNewspaper();
+      }
    }
-   
+    
+   private void PickUpNewspaper()
+   {
+      if (newspaperText != null) newspaperText.gameObject.SetActive(true);
+      if (newspaperImage != null) newspaperImage.gameObject.SetActive(true);
+        
+      NewspaperPickedUp = true; // Set the static flag
+      Destroy(gameObject);
+      
+   }
 }
